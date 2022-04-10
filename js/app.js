b9380f49da0602e9a438235f0d166bbdbac1f6c5
UttamSaha <uttamsaha982@gmail.com>
2022-04-10 21:42:07 +0600

    css & design added

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 document.getElementById("selectbox").addEventListener("click", () => {
      const inputs = document.getElementById("inputs");
      inputs.innerHTML = '';
+     document.getElementById('table').innerHTML = '';
+     document.getElementById("result-text").innerText = '';
 
   console.log('test');
 
@@ -20,6 +22,7 @@ document.getElementById("selectbox").addEventListener("click", () => {
 });
 
 document.getElementById('btn').addEventListener('click',()=>{
+    document.getElementById("result-text").innerText = 'Result: '
     const select = document.getElementById("selectbox").value;
     const xValue = document.getElementById('x').value;
     const x = parseInt(xValue);
@@ -28,15 +31,18 @@ document.getElementById('btn').addEventListener('click',()=>{
 
 
     const table = document.getElementById('table');
+    table.style.backgroundColor = "rgba(255, 86, 56, 0.43)";
+    table.style.boxShadow = 'rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px';
     table.innerHTML = '';
     table.style.gridTemplateColumns = `repeat(${result},100px)`
     for(let i = 1; i <= result*result; i++){
         let indexValue = document.getElementById(i).value;
         const evalResult = eval(indexValue);
+        const xx = evalResult.toFixed(3)
         const tr = document.createElement('tr');
         tr.className = "table-row";
         tr.innerHTML = `
-        <td style="border:1px solid black";padding:10px;>${evalResult}</td>
+        <td style="border:0px solid yellow; padding:20px;border-radius:10px;margin:20px;box-shadow: rgba(0, 0, 0, 0.07) 0px 1px 2px, rgba(0, 0, 0, 0.07) 0px 2px 4px, rgba(0, 0, 0, 0.07) 0px 4px 8px, rgba(0, 0, 0, 0.07) 0px 8px 16px, rgba(0, 0, 0, 0.07) 0px 16px 32px, rgba(0, 0, 0, 0.07) 0px 32px 64px;background-color: rgba(255, 86, 56, 0.5);font-weight:550;">${evalResult}</td>
         `;
 
         table.appendChild(tr);
